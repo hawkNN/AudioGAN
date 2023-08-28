5cf94e67f51cc7c10ffb6f41abc7058eb70db2dd
hawkNN <84937637+hawkNN@users.noreply.github.com>
2023-08-28 18:15:32 -0400

    modified animate_MELspect
    working through approach, will be in view_mels.py in the end

--- animate_MELspect2.py
+++ animate_MELspect2.py
@@ -3,13 +3,16 @@ import librosa
 import matplotlib.pyplot as plt
 from IPython.display import Audio, display
 from matplotlib.animation import FuncAnimation
+from moviepy.editor import VideoFileClip, AudioFileClip
+
 
 def animate_mel_spectrogram(audio_file_path, output_video_path=None):
     # Load audio wave file
     y, sr = librosa.load(audio_file_path, sr=None)
+    print(f'sampling rate is: {sr}')
 
     # Compute mel spectrogram
-    mel_spectrogram = librosa.feature.melspectrogram(y, sr=sr)
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
     mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
 
     # Create figure and axis for animation
@@ -29,8 +32,8 @@ def animate_mel_spectrogram(audio_file_path, output_video_path=None):
     # Create the animation
     ani = FuncAnimation(fig, update, frames=total_frames, blit=True, repeat=False)
 
-    # Display the animation synchronized with audio
-    audio_display = display(Audio(y, rate=sr, autoplay=True), ani)
+    # # Display the animation synchronized with audio
+    # audio_display = display(Audio(y, rate=sr, autoplay=True), ani)
 
     if output_video_path:
         output_video_path = output_video_path.lower()
@@ -42,13 +45,25 @@ def animate_mel_spectrogram(audio_file_path, output_video_path=None):
 
     plt.show()
 
-def play_audio(audio_file_path):
-    display(Audio(filename=audio_file_path, autoplay=True))
+def combine_audio_and_video(audio_file_path, 
+                            video_file_path, 
+                            output_video_path,
+                            sampling_rate=22500):
+    audio_clip = AudioFileClip(audio_file_path, fps=sampling_rate)
+    video_clip = VideoFileClip(video_file_path)
+    
+    # Set audio of the video to the provided audio clip
+    video_clip = video_clip.set_audio(audio_clip)
+    
+    # Write the combined video to the output path
+    video_clip.write_videofile(output_video_path, codec='libx264', audio_codec='aac')
+
 
 # Usage
 audio_file_path = 'speech_clips/audioclipglory1_1.mp3'
-output_video_path = '../gitOutbox/audioclipglory1_1.mp4' # Optional, set to None if not saving
-play_audio(audio_file_path)
-
-# animate_mel_spectrogram(audio_file_path, output_video_path)
+video_file_path = '../gitOutbox/temp.mp4' # Optional, set to None if not saving
+av_file_path = '../gitOutbox/audioclipglory1_1.mp4'
+# play_audio(audio_file_path)
+animate_mel_spectrogram(audio_file_path, video_file_path)
+combine_audio_and_video(audio_file_path,video_file_path,av_file_path)
 
