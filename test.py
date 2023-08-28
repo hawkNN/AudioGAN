aa4971d2d564f9188370231ef633295b6c605c41
hawkNN <84937637+hawkNN@users.noreply.github.com>
2023-08-28 18:16:47 -0400

    Update test.py
    just a garbage test file

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-
-import os
-import numpy as np
-
-test_path = '../bigvgan'
-print(os.listdir(test_path))
-print(os.path.exists(test_path))
-
-# from pydub import AudioSegment
-# from pydub.playback import play
-# import threading
-
-# sound = AudioSegment.from_mp3('speech_clips\audioclipglory1_1.mp3')
-# t = threading.Thread(target=play, args=(sound,))
-# t.start()
-
-# print("I would like this line to be executed simultaneously with the audio playing")
-
-
-print(f'937/10 is {np.floor_divide(937,10)}')
+from playsound import playsound
+audio_file_path = 'speech_clips/audioclipglory1_1.mp3'
+playsound(audio_file_path)
